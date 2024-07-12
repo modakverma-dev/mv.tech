6f87050be567564d902fb514cf72d23cbaf3f4a1
modakverma-tmpl <modak.v@tunica.tech>
2024-07-12 12:51:59 +0530

    css load issue config update

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 
 const { withContentlayer } = require("next-contentlayer");
 
-const nextConfig = {
-  output: "export",
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  basePath: isProd ? "/MV.Tech" : "",
+  assetPrefix: isProd ? "/MV.Tech/" : "",
+  trailingSlash: true,
 };
 
 module.exports = withContentlayer({ ...nextConfig });
