2c37854506aaded74a2f09bcbb763a286b24f8bc
modakverma-tmpl <modak.v@tunica.tech>
2024-07-19 15:07:11 +0530

    logo update

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,10 +1,22 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import profileImg from "@/public/profile.jpg";
 import { cx } from "@/src/utils";
+import { motion } from "framer-motion";
 
 const Logo = ({ nav }) => {
+  const icon = {
+    hidden: {
+      pathLength: 0,
+      fill: "rgba(0, 0, 0, 1)",
+    },
+    visible: {
+      pathLength: 1,
+      fill: "rgba(255, 255, 255, 1)",
+    },
+  };
   return (
     <Link
       href="/"
@@ -21,11 +33,23 @@ const Logo = ({ nav }) => {
       </div>
       <span
         className={cx(
-          "font-bold text-xl mix-blend-multiply",
+          "font-bold text-xl mix-blend-multiply flex items-center gap-1",
           nav ? "text-black" : "text-white"
         )}
       >
-        MV.TECH
+        <svg
+          className="w-4 h-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+        >
+          <motion.path
+            d="M0 100V0l50 50 50-50v100L75 75l-25 25-25-25z"
+            variants={icon}
+            initial={!nav ? "hidden" : "visible"}
+            animate={!nav ? "visible" : "hidden"}
+          />
+        </svg>
+        V.TECH
       </span>
     </Link>
   );
